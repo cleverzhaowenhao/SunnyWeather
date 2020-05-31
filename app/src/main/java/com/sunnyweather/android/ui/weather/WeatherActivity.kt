d4db9c6c54b17c7dd44bcf0d5cebd2b2adb0e618
Andy <vanhaover@163.com>
2020-05-31 12:42:13 +0800

    效果不佳，取消状态栏设置；

--- app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
@@ -31,10 +31,6 @@ class WeatherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val decorView = window.decorView
-        decorView.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        window.statusBarColor = Color.TRANSPARENT
         setContentView(R.layout.activity_weather)
 
         if (viewModel.locationLng.isEmpty()) {
